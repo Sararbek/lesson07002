9c54170053e41b09f8fc824db4da78c2c7250b0d
sarvarbek_erkinov <93415553+Sararbek@users.noreply.github.com>
2025-02-15 18:18:38 +0500

    z-50

--- src/utils/SuspenseContainer.jsx
+++ src/utils/SuspenseContainer.jsx
@@ -3,7 +3,7 @@ import loadingGif from "@/assets/loading_gif.gif"
 
 const Loading = () => {
     return (
-        <div className="fixed top-0 left-0 w-full h-screen bg-black">
+        <div className="fixed top-0 left-0 w-full h-screen bg-black z-50">
             <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  ">
                 <img src={loadingGif} alt="" />
             </div>
